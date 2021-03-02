e77a40b38a01d5d1358ec6997926d78fa6ae5481
PythonEESD2021 <ctran@eesd.org>
2021-03-02 00:41:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+'''
+03/01/2021
+
+List
+
+A data structure that is a mutable (changeable) ordered sequence of elements. Defined by having values between square brakets, [ ].
+
+student1 = "John"
+
+student2 = ["John", "Chloe", "Ian", "Ashley"]
+print (student2)
+>> ["John", "Chloe", "Ian", "Ashley"]
+
+box = ["Anything", 123, True, 4.28, -13]
+print (box)
+>> ["Anything", 123, True, 4.28, -13]
+
+
+Indexing List
+Each item in a list corresponds to an index number, which uses an integer; starting with 0.
+
+print (box[1])
+
+
+List Functions
+
+adding elements/items
+1. LIST.append(ELEMENT)
+2. LIST.insert(INDEX, ELEMENT)
+
+'''
+
+box = ["Anything", 123, True, 4.28, -13]
+box.append("toy")
+box.insert(1, False)
+
+print (box)
